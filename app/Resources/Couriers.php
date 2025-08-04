434b37f07f07c25b37d22d75b1570db1e111737f
Ishan Kukadia <8424868+ishan576@users.noreply.github.com>
2025-08-04 15:42:52 +0530

    Update Couriers.php

--- app/Resources/Couriers.php
+++ app/Resources/Couriers.php
@@ -11,38 +11,40 @@ trait Couriers
 	 * @param 	string 		$delivery_postcode
 	 * @param 	bool 		$is_cod
 	 * @param 	float 		$weight
-	 * @param 	string 		$shiprocket_order_id
+	 * @param 	string 		$mode
 	 * @return 	void
 	 */
-    public function checkServiceability(
+	public function checkServiceability(
 		$pickup_postcode, 
 		$delivery_postcode, 
-		$is_cod = 0, 
 		$weight = 0, 
-		$shiprocket_order_id = 0
+		$is_cod = 0, 
+		$mode
 	) {
-        return $this->request(
+		return $this->request(
 			'get', 
 			'courier/serviceability?' .
 				'pickup_postcode=' . $pickup_postcode . '&' .
 				'delivery_postcode=' . $delivery_postcode . '&' .
 				'weight=' . $weight . '&' .
 				'cod=' . $is_cod . '&' .
-				'order_id=' . $shiprocket_order_id
+				'mode=' . $mode
 		);
 	}
 	
 	public function assignAWBs(
 		$shipment_ids = [],
 		$courier_id,
-		$weight
+		$re_assign = false
 	) {
 		if (count($shipment_ids) > 1) {
 			return $this->request(
 				'post', 
 				'courier/assign/awb',
 				[
-					'shipment_id' => $shipment_ids
+					'shipment_id' => $shipment_ids,
+					'courier_id' => $courier_id,
+					'status' => ($re_assign ? 'reassign' : '')
 				]
 			);
 		}
@@ -53,12 +55,12 @@ trait Couriers
 			[
 				'shipment_id' => $shipment_ids,
 				'courier_id' => $courier_id,
-				'weight' => $weight
+				'status' => ($re_assign ? 'reassign' : '')
 			]
 		);
 	}
 
-    /**
+	/**
 	 * generate label
 	 *
 	 * @param array $shipment_ids
@@ -70,26 +72,26 @@ trait Couriers
 	{
 		return $this->request('post', 'courier/generate/label', ['shipment_id' => $shipment_ids]);
 	}
-	
+
 	public function getInvoice(array $shipment_ids)
 	{
 		return $this->request('post', 'orders/print/invoice', ['ids' => $shipment_ids]);
 	}
-	
+
 	public function requestPickup(array $shipment_ids)
 	{
 		return $this->request('post', 'courier/generate/pickup', ['shipment_id' => $shipment_ids]);
 	}
 
-    /**
-     * Makes a request to the Shiprocket API and returns the response.
-     *
-     * @param    string $verb       The Http verb to use
-     * @param    string $path       The path of the APi after the domain
-     * @param    array  $parameters Parameters
-     *
-     * @return   stdClass The JSON response from the request
-     * @throws   Exception
-     */
-    abstract protected function request($verb, $path, $parameters = []);
+	/**
+	 * Makes a request to the Shiprocket API and returns the response.
+	 *
+	 * @param    string $verb       The Http verb to use
+	 * @param    string $path       The path of the APi after the domain
+	 * @param    array  $parameters Parameters
+	 *
+	 * @return   stdClass The JSON response from the request
+	 * @throws   Exception
+	 */
+	abstract protected function request($verb, $path, $parameters = []);
 }
